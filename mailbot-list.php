9ee37365c68116cad5c69d8e34f51d4433132a17
Daniele Costarella <daniele.costarella@yahoo.it>
2014-09-19 16:50:39 +0200

    Log file removed; removed unnecessary comments

--- mailbot-list.php
+++ mailbot-list.php
@@ -70,48 +70,6 @@ foreach ($groups as $key => $value)
 		}
 	Print "</table>";
 	}
-
-/*
-// creates 'users' table
-//
-$query = "CREATE TABLE IF NOT EXISTS `" . $db_name . "`.`mailbot_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(100) NOT NULL DEFAULT '',
-  `user_email` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`));";
-// Execute query
-if (mysql_query($query))
-	echo "<br>Table 'mailbot_users' created successfully";
-else
-	echo "<br>Error creating table: " . mysql_error();
-
-
-// creates 'groups' table
-//
-$query = "CREATE TABLE IF NOT EXISTS `" . $db_name . "`.`mailbot_groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `group_name` varchar(30) NOT NULL DEFAULT '',
-  `group_description` varchar(160) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`));";
-// Execute query
-if (mysql_query($query))
-	echo "<br>Table 'mailbot_groups' created successfully";
-else
-	echo "<br>Error creating table: " . mysql_error();
-
-
-// creates 'groups_users' table
-//
-$query = "CREATE TABLE IF NOT EXISTS `" . $db_name . "`.`mailbot_groups_users` (
-  `group_id` int(11) NOT NULL DEFAULT 0,
-  `user_id` int(11) NOT NULL DEFAULT 0);";
-// Execute query
-if (mysql_query($query))
-	echo "<br>Table 'mailbot_groups_users' created successfully";
-else
-	echo "<br>Error creating table: " . mysql_error();
-*/
-
-// closes connection
+	
 mysql_close($db);
 ?>
